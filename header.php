6be63ac2cf157c74dde42f9f4cb4e1670e3536f5
kaimouk <hathairatcoppywork@gmail.com>
2018-02-12 14:02:58 +0700

    update

---
+++ header.php
@@ -0,0 +1,35 @@
+<div id="top-bar" class="container">
+			<div class="row">
+				<div class="span4">
+					<form method="POST" class="search_form">
+						<input type="text" class="input-block-level search-query" Placeholder="eg. T-sirt">
+					</form>
+				</div>
+				<div class="span8">
+					<div class="account pull-right">
+						<ul class="user-menu">				
+							<li><a href="#">My Account</a></li>
+							<li><a href="cart.php">Your Cart</a></li>
+							<li><a href="checkout.php">Checkout</a></li>					
+							<li><a href="register.php">Login</a></li>		
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="wrapper" class="container">
+			<section class="navbar main-menu">
+				<div class="navbar-inner main-menu">				
+					<a href="index.php" class="logo pull-left"><img src="themes/images/logo.png" class="site_logo" alt=""></a>
+					<nav id="menu" class="pull-right">
+						<ul>
+							<li><a href="./products.php">Cake</a></li>															
+							<li><a href="./products.php">Cookie</a></li>			
+							<li><a href="./products.php">Bread</a></li>							
+							<li><a href="./products.php">Pie</a></li>
+							<li><a href="./products.php">Danish</a></li>
+							<li><a href="./products.php">Tarts</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>	
---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+		<!-- bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">      
+		<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+		
+		<link href="themes/css/bootstrappage.css" rel="stylesheet"/>
+		
+		<!-- global styles -->
+		<link href="themes/css/flexslider.css" rel="stylesheet"/>
+		<link href="themes/css/main.css" rel="stylesheet"/>
+
+		<!-- scripts -->
+		<script src="themes/js/jquery-1.7.2.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>				
+		<script src="themes/js/superfish.js"></script>	
+		<script src="themes/js/jquery.scrolltotop.js"></script>
+		<!--[if lt IE 9]>			
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+    <body>		
+		
+
+	<?php include("header.php");?>
+	<section  class="homepage-slider" id="home-slider">
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							<img src="themes/images/carousel/banner-1.png" alt="" />
+						</li>
+						<li>
+							<img src="themes/images/carousel/banner-2.png" alt="" />
+							<!-- <div class="intro">
+								<h1>Mid season sale</h1>
+								<p><span>Up to 50% Off</span></p>
+								<p><span>On selected items online and in stores</span></p>
+							</div> -->
+						</li>
+					</ul>
+				</div>			
+			</section>
+			<section class="header_text">
+				ไขเคล็ดลับที่ผูกใจคนทุกเพศทุกวัยมากว่า 80 ปี! ปัจจุบัน ร้านปั้นลี่เบเกอรี่ เพิ่มความสะดวกสบายเพื่อเข้าถึงลูกค้าทุกกลุ่มด้วย 
+				<br/>บริการจัดเบรค อาหารว่าง สแน็คบ๊อกซ์ พร้อมบริการ สั่งซื้อออนไลน์ และเดลิเวอรี่ถึงที่ ทั่ว กทม. และปริมณฑล ... 
+			</section>
+			<section class="main-content">
+				<div class="row">
+					<div class="span12">													
+						<div class="row">
+							<div class="span12">
+								<h4 class="title">
+									<span class="pull-left"><span class="text"><span class="line">Feature <strong>Products</strong></span></span></span>
+									<span class="pull-right">
+										<a class="left button" href="#myCarousel" data-slide="prev"></a><a class="right button" href="#myCarousel" data-slide="next"></a>
+									</span>
+								</h4>
+								<div id="myCarousel" class="myCarousel carousel slide">
+									<div class="carousel-inner">
+										<div class="active item">
+											<ul class="thumbnails">												
+												<li class="span3">
+													<div class="product-box">
+														<span class="sale_tag"></span>
+														<p><a href="product_detail.php"><img src="themes/images/product/product11.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Ut wisi enim ad</a><br/>
+														<a href="products.php" class="category">Commodo consequat</a>
+														<p class="price">$17.25</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<span class="sale_tag"></span>
+														<p><a href="product_detail.php"><img src="themes/images/product/product12.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Quis nostrud exerci tation</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$32.50</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product13.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Know exactly turned</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$14.20</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product15.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">You think fast</a><br/>
+														<a href="products.php" class="category">World once</a>
+														<p class="price">$31.45</p>
+													</div>
+												</li>
+											</ul>
+										</div>
+										<div class="item">
+											<ul class="thumbnails">
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product16.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Know exactly</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$22.30</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product17.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Ut wisi enim ad</a><br/>
+														<a href="products.php" class="category">Commodo consequat</a>
+														<p class="price">$40.25</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product2.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">You think water</a><br/>
+														<a href="products.php" class="category">World once</a>
+														<p class="price">$10.45</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product6.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Quis nostrud exerci</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$35.50</p>
+													</div>
+												</li>																																	
+											</ul>
+										</div>
+									</div>							
+								</div>
+							</div>						
+						</div>
+						<br/>
+						<div class="row">
+							<div class="span12">
+								<h4 class="title">
+									<span class="pull-left"><span class="text"><span class="line">Latest <strong>Products</strong></span></span></span>
+									<span class="pull-right">
+										<a class="left button" href="#myCarousel-2" data-slide="prev"></a><a class="right button" href="#myCarousel-2" data-slide="next"></a>
+									</span>
+								</h4>
+								<div id="myCarousel-2" class="myCarousel carousel slide">
+									<div class="carousel-inner">
+										<div class="active item">
+											<ul class="thumbnails">												
+												<li class="span3">
+													<div class="product-box">
+														<span class="sale_tag"></span>
+														<p><a href="product_detail.php"><img src="themes/images/product/product8.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Ut wisi enim ad</a><br/>
+														<a href="products.php" class="category">Commodo consequat</a>
+														<p class="price">$25.50</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product9.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Quis nostrud exerci tation</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$17.55</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product2.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Know exactly turned</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$25.30</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product3.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">You think fast</a><br/>
+														<a href="products.php" class="category">World once</a>
+														<p class="price">$25.60</p>
+													</div>
+												</li>
+											</ul>
+										</div>
+										<div class="item">
+											<ul class="thumbnails">
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product6.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Know exactly</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$45.50</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product8.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Ut wisi enim ad</a><br/>
+														<a href="products.php" class="category">Commodo consequat</a>
+														<p class="price">$33.50</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product9.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">You think water</a><br/>
+														<a href="products.php" class="category">World once</a>
+														<p class="price">$45.30</p>
+													</div>
+												</li>
+												<li class="span3">
+													<div class="product-box">
+														<p><a href="product_detail.php"><img src="themes/images/product/product15.jpg" alt="" /></a></p>
+														<a href="product_detail.php" class="title">Quis nostrud exerci</a><br/>
+														<a href="products.php" class="category">Quis nostrud</a>
+														<p class="price">$25.20</p>
+													</div>
+												</li>																																	
+											</ul>
+										</div>
+									</div>							
+								</div>
+							</div>						
+						</div>
+						<div class="row feature_box">						
+							<div class="span4">
+								<div class="service">
+									<div class="responsive">	
+										<img src="themes/images/feature_img_2.png" alt="" />
+										<h4>MODERN <strong>DESIGN</strong></h4>
+										<p>Lorem Ipsum is simply dummy text of the printing and printing industry unknown printer.</p>									
+									</div>
+								</div>
+							</div>
+							<div class="span4">	
+								<div class="service">
+									<div class="customize">			
+										<img src="themes/images/feature_img_1.png" alt="" />
+										<h4>FREE <strong>SHIPPING</strong></h4>
+										<p>Lorem Ipsum is simply dummy text of the printing and printing industry unknown printer.</p>
+									</div>
+								</div>
+							</div>
+							<div class="span4">
+								<div class="service">
+									<div class="support">	
+										<img src="themes/images/feature_img_3.png" alt="" />
+										<h4>24/7 LIVE <strong>SUPPORT</strong></h4>
+										<p>Lorem Ipsum is simply dummy text of the printing and printing industry unknown printer.</p>
+									</div>
+								</div>
+							</div>	
+						</div>		
+					</div>				
+				</div>
+			</section>
+			<section class="our_client">
+				<h4 class="title"><span class="text">Manufactures</span></h4>
+				<div class="row">					
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/14.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/35.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/1.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/2.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/3.png"></a>
+					</div>
+					<div class="span2">
+						<a href="#"><img alt="" src="themes/images/clients/4.png"></a>
+					</div>
+				</div>
+			</section>
+			<section id="footer-bar">
+				<div class="row">
+					<div class="span3">
+						<h4>Navigation</h4>
+						<ul class="nav">
+							<li><a href="./index.php">Homepage</a></li>  
+							<li><a href="./about.php">About Us</a></li>
+							<li><a href="./contact.php">Contac Us</a></li>
+							<li><a href="./cart.php">Your Cart</a></li>
+							<li><a href="./register.php">Login</a></li>							
+						</ul>					
+					</div>
+					<div class="span4">
+						<h4>My Account</h4>
+						<ul class="nav">
+							<li><a href="#">My Account</a></li>
+							<li><a href="#">Order History</a></li>
+							<li><a href="#">Wish List</a></li>
+							<li><a href="#">Newsletter</a></li>
+						</ul>
+					</div>
+					<div class="span5">
+						<p class="logo"><img src="themes/images/logo.png" class="site_logo" alt=""></p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. the  Lorem Ipsum has been the industry's standard dummy text ever since the you.</p>
+						<br/>
+						<span class="social_icons">
+							<a class="facebook" href="#">Facebook</a>
+							<a class="twitter" href="#">Twitter</a>
+							<a class="skype" href="#">Skype</a>
+							<a class="vimeo" href="#">Vimeo</a>
+						</span>
+					</div>					
+				</div>	
+			</section>
+			<section id="copyright">
+				<span>Copyright 2013 bootstrappage template  All right reserved.</span>
+			</section>
+		</div>
+		<script src="themes/js/common.js"></script>
+		<script src="themes/js/jquery.flexslider-min.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				$(document).ready(function() {
+					$('.flexslider').flexslider({
+						animation: "fade",
+						slideshowSpeed: 4000,
+						animationSpeed: 600,
+						controlNav: false,
+						directionNav: true,
+						controlsContainer: ".flex-container" // the container that holds the flexslider
+					});
+				});
+			});
+		</script>
+    </body>
+</html>
